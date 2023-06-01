ba50a34d43fc4a4e62256f450c5a1aa520439c3d
GustavoOM <gumartins2001@gmail.com>
2023-06-01 00:07:05 -0300

    Carregar os dados na inicialização da pagina

--- src/pages/PosGrad.vue
+++ src/pages/PosGrad.vue
@@ -1,33 +1,46 @@
 <template>
   <div>
     <Cabecalho/>
-    <ListaFichasIndividuais/>
+    <ListaFichasIndividuais listaCompleta/>
     <Rodape/>
   </div>
 </template>
 
 <script>
-  import axios from "axios";
 
   import Cabecalho from '../components/Cabecalho.vue'
   import ListaFichasIndividuais from '../components/ListaFichasIndividuais.vue'
   import Rodape from '../components/Rodape.vue'
 
-  axios
-  .get("http://thanos.icmc.usp.br:4567/api/v1/defesas")
-  .then((res) => {
-          console.log(res.data)
-  })
-  .catch((error) => {
-          console.log(error);
-  });
-
   export default{
     name: 'PosGrad',
+    data() {
+      return {
+        alunoAtivo: undefined,
+        filter: '',
+        alunosPosGraduacao: {}
+      };
+    },
     components:{
       Cabecalho,
       ListaFichasIndividuais,
       Rodape
+    },
+    methods: {
+      carregarAlunosPosGraduacao() {
+      const url = 'http://thanos.icmc.usp.br:4567/api/v1/defesas';
+      fetch(url)
+        .then((data) => (data.json()))
+        .then((response) => {
+          this.alunosPosGraduacao = response;
+          console.log(JSON.stringify(this.alunosPosGraduacao))
+        });
+      },
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.carregarAlunosPosGraduacao()
+        })
     }
   }
 </script>
