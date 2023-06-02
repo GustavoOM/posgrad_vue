fef8892cdcdaa24656ef9040b755016139a7c969
GustavoOM <gumartins2001@gmail.com>
2023-06-02 18:40:48 -0300

    Filtros e Ficha Selecionada

---
+++ src/components/FichaIndividualSelecionada.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="fundo">
+        <h1 class="nome">{{nome}}</h1>
+        <h2 class="data">{{data}}</h2>
+        <div class="cursoPrograma">
+            <h3>Curso: {{curso}}</h3>
+            <h3>Programa: {{programa}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "FichaIndividualSelecionada",
+        props: {
+            nome: String,
+            data: String,
+            curso: String,
+            programa: String
+        }
+    }
+</script>
+
+<style scoped>
+    .fundo{
+        color: #FEA342;
+        margin: 20px;
+        border-radius: 10px;
+        background-color: #3d3d3d;
+        border: 4px solid #FEA342;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .nome{
+        text-align: center;
+        font-family: "Roboto", sans-serif;
+
+    }
+    .data{
+        margin: -5px;
+        text-align: center;
+        font-family: "Roboto", sans-serif;
+
+    }
+    .cursoPrograma{
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        gap: 25px;
+        font-family: "Roboto", sans-serif;
+
+    }
+</style>
--- src/components/ListaFichasIndividuais.vue
+++ src/components/ListaFichasIndividuais.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="conteudo">
       <FiltrosEBuscas/>
+      <FichaIndividualSelecionada nome="Hildebrando Munhoz Rodrigues" data="02/06/2023" curso="BSI" programa="em Vue"/>
       <div id="itens">
-        <FichaIndividual :nome="posgrad.Nome" v-for="(posgrad, index) in listaPosGrad" :key="index"/>
+        <FichaIndividual :nome="posgrad.Nome" v-for="posgrad in listaPosGrad" :key="posgrad.Ordem"/>
       </div>
     </div>
   </template>
@@ -10,12 +11,26 @@
   <script>
   import FichaIndividual from '../components/FichaIndividual.vue'
   import FiltrosEBuscas from '../components/FiltrosEBuscas.vue'
+  import FichaIndividualSelecionada from '../components/FichaIndividualSelecionada.vue'
   
   export default {
     name: "ListaFichasIndividuais",
     components: {
       FichaIndividual,
-      FiltrosEBuscas
+      FiltrosEBuscas,
+      FichaIndividualSelecionada
+    },
+    data() {
+        return {
+            personagem: undefined,
+            filter: '',
+            personagens: [],
+            loading: false,
+            cursoSelecionado: "",
+            programaSelecionado: "",
+            ordemSelecionado: "ANOC",
+            busca: "",
+        };
     },
     props: {
       listaPosGrad: {
@@ -27,15 +42,31 @@
   </script>
   
   <style scoped>
+  #conteudo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   #itens {
     background-color: #597897;
     border-radius: 20px;
-    margin: 50px 20px 50px 20px;
+    margin: 0px 20px;
     padding: 20px;
     display: grid;
     grid-template-columns: repeat(4, minmax(0, 1fr));
     gap: 20px;
     align-items: center;
+  }
+  @media (max-width: 1000px) {
+    #itens{
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+  }
+
+  @media (max-width: 550px) {
+    #itens{
+        grid-template-columns: repeat(1, minmax(0, 1fr));
+    }
   }
   </style>
   
