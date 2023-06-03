25d841783204d91ade7b173d1c8623e13accde27
EduardoMirandaz <eduardo.cmazevedo@gmail.com>
2023-06-03 20:54:12 -0300

    Titulo da pagina e botao de voltar para a home adicionados.

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,5 +1,14 @@
 <template>
-    <h1>Cabecalho</h1>
+    <header>
+        <div class="header-content">
+            <h1 class="header-title">Lista de fichas dos alunos de pós graduação</h1>
+            <a href="#" class="header-back-button">
+                <span class="home-icon"></span>
+                Home
+            </a>
+        </div>
+    </header>
+
 </template>
 
 <script>
@@ -7,3 +16,45 @@
         name: "Cabecalho"
     }
 </script>
+
+<style scoped>
+
+    header {
+        background-color: #414A53;
+        padding: 20px;
+    }
+
+    .header-content {
+        display: flex;
+        align-items: center;
+    }
+
+    .header-back-button {
+        display: flex;
+        align-items: center;
+        background-color: #FFFFFF;
+        padding: 10px;
+        border-radius: 4px;
+        text-decoration: none;
+        color: #414A53;
+        margin-right: 20px;
+        position: absolute;
+        right: 0;
+    }
+
+    .home-icon {
+        width: 1.25rem;
+        height: 1.25rem;
+        background-image: url(../../src/assets/botaoHome.svg);
+        background-size: cover;
+        margin-right: 5px;
+    }
+
+    .header-title {
+        color: #FFFFFF;
+        margin: 0;
+        text-align: center;
+    }
+
+
+</style>
